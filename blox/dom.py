b3963899bc91ac25430d87c464c3112e3a6fed36
Timothy Crosley <timothy.crosley@gmail.com>
2015-11-08 15:12:48 -0800

    Tag->DOM

--- blox/dom.py
+++ blox/dom.py
@@ -19,6 +19,8 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFT
 OTHER DEALINGS IN THE SOFTWARE.
 
 '''
-from blox.base import Tag
+from blox.base import Tag, NamedTag
 
-class DOM
+
+class DOM(Tag):
+    pass
