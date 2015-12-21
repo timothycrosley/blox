00af6cfe47b14a7bc11aadcf33631232b33cb3b1
Timothy Crosley <timothy.crosley@gmail.com>
2015-12-20 17:38:35 -0800

    Continue work on compilation

--- blox/compile.py
+++ blox/compile.py
@@ -77,8 +77,6 @@ def to_python(template, factory=factory, indent=0, indentation='    '):
     python = []
     elements_used = set()
     accessors_used = set()
-    cache_elements = set()
-    static_elements = set()
     instance = 0
     parent_node = "template"
 
@@ -86,114 +84,105 @@ def to_python(template, factory=factory, indent=0, indentation='    '):
         if not template:
             return
 
+        indented = indentation * indent
         instance += 1
         new_node = "element" + str(instance)
         if type(template) in (str, unicode):
-            python.append("\n%s%s = %s.add(" % (indented, newNode, parent_node))
-            python.append('Text("' + template + '"), ensureUnique=False)')
+            python.append("\n%s%s = %s(" % (indented, newNode, parent_node))
+            python.append('Text("' + template + '"))')
 
 
-    (accessor, id, name, create, properties, children) = (template.accessor, template.id, template.name,
-                                                          template.create, template.properties, template.childElements)
-    elements_used.add(create)
-    element = factory.products[create]
-    create = create.replace("-", "_")
-    if create in ("and", "or", "with", "if", "del", "template"):
-        create = "_" + create
 
-    accessor = accessor or id
-    if accessor:
-        accessor = accessor.replace("-", "_")
+        (accessor, id, name, create, properties, children) = (template.accessor,
+                                                              template.id, template.name,
+                                                              template.create, template.properties, template.childElements)
+        elements_used.add(create)
 
-    isCached = False
-    if create.endswith("cacheelement"):
-        cache_elements.add(newNode)
-        isCached = True
-        python += "\n%s%s = globals()['%s']" % (indented, newNode, newNode)
-        python += "\n%sif not %s.rendered():" % (indented, newNode)
-        python += "\n%s%s = CacheElement()" % (indented + INDENT, newNode)
-        python += "\n%sglobals()['%s'] = %s" % (indented + INDENT, newNode, newNode)
-    elif create in ("static", "display-static") and parent_node != "template":
-        html = CompiledTemplate.create(template, factory).build(factory).toHTML()
-        static_elements.add('%s = StraightHTML(html="""%s""")' % (newNode, html))
+        element = factory.products[create]
+        create = create.replace("-", "_")
+        if create in ("and", "or", "with", "if", "del", "template"):
+            create = "_" + create
+
+        accessor = accessor or id
+        if accessor:
+            accessor = accessor.replace("-", "_")
+
+        python += '\n{indented}{1} = {2}(id={3}, name={4}, parent={5})'.format(
+                    (indented, newNode, create.lower(), repr(id),
+                                                                repr(name), parent_node)
+        for name, value in properties:
+            if value is not None and name in element.properties:
+                propertyDict = element.properties[name]
+                propertyActions = propertyDict['action'].split('.')
+                propertyAction = propertyActions.pop(-1)
+                if propertyActions:
+                    propertyActions = "." + ".".join(propertyActions)
+                else:
+                    propertyActions = ""
+                propertyName = propertyDict.get('name', name)
+
+                if propertyAction == "classAttribute":
+                    python += "\n%s%s%s.%s = %s" % (indented, newNode, propertyActions, propertyName, repr(value))
+                elif propertyAction == "attribute":
+                    python += "\n%s%s%s.attributes[%s] = %s" % (indented, newNode, propertyActions, repr(propertyName),
+                                                                repr(value))
+                elif propertyAction == "javascriptEvent":
+                    python += "\n%s%s%s.addJavascriptEvent(%s, %s)" % (indented, newNode, propertyActions,
+                                                                    repr(propertyName), repr(value))
+                elif propertyAction == "call":
+                    if value:
+                        python += "\n%s%s%s.%s()" % (indented, newNode, propertyActions, propertyName)
+                elif propertyAction == "send":
+                    python += "\n%s%s%s.%s(%s, %s)" % (indented, newNode, propertyActions,
+                                                                    propertyName, repr(name), repr(value))
+                elif propertyAction == "addClassesFromString":
+                    python += "\n%s%s%s.addClasses(%s)" % (indented, newNode, propertyActions,
+                                                        repr(tuple(value.split(" "))))
+                elif propertyAction == "setStyleFromString":
+                    python += "\n%s%s%s.style.update(%s)" % (indented, newNode, propertyActions,
+                                                            repr(StyleDict.fromString(value)))
+                else:
+                    python += "\n%s%s%s.%s(%s)" % (indented, newNode, propertyActions, propertyAction, repr(value))
+
+        if accessor:
+            accessors_used.add(accessor)
+            python += "\n%stemplate.%s = %s" % (indented, accessor, newNode)
+
+        if children:
+            if isCached:
+                childAccessors = set()
+                childIndent = indent + 1
+            else:
+                childAccessors = accessors_used
+                childIndent = indent
+            for node in children:
+                (childPython, instance) = __createPythonFromTemplate(node, factory, newNode, instance, elements_used,
+                                                                childAccessors, cache_elements, static_elements, childIndent)
+                python += childPython
+            if isCached:
+                if childAccessors:
+                    accessors_used.update(childAccessors)
+                    python += "\n%selse:" % indented
+                    for accessor in childAccessors:
+                        python += "\n%stemplate.%s = %s" % (indented + INDENT, accessor, newNode)
+
+
         python += "\n%s%s.add(%s, ensureUnique=False)" % (indented, parent_node, newNode)
+        if parent_node == "template":
+            defineElements = ""
+            for elementName in elements_used:
+                variableName = elementName.replace("-", "_")
+                if variableName in ("and", "or", "with", "if", "del", "template"):
+                    variableName = "_" + variableName
+                defineElements += "globals()['%s'] = products['%s']\n%s" % (variableName, elementName, INDENT * 2)
+
+            cacheDefinitions = ""
+            for elementName in cache_elements:
+                cacheDefinitions += "%s = CacheElement()\n" % elementName
+
+            return SCRIPT_TEMPLATE % {'accessors':tuple(accessors_used), 'buildTemplate':python,
+                                    'defineElements':defineElements, 'cache_elements':cacheDefinitions,
+                                    'static_elements':"\n".join(static_elements)}
+
+
         return (python, instance)
-    else:
-        python += '\n%s%s = %s(id=%s, name=%s, parent=%s)' % (indented, newNode, create.lower(), repr(id),
-                                                              repr(name), parent_node)
-    for name, value in properties:
-        if value is not None and name in element.properties:
-            propertyDict = element.properties[name]
-            propertyActions = propertyDict['action'].split('.')
-            propertyAction = propertyActions.pop(-1)
-            if propertyActions:
-                propertyActions = "." + ".".join(propertyActions)
-            else:
-                propertyActions = ""
-            propertyName = propertyDict.get('name', name)
-
-            if propertyAction == "classAttribute":
-                python += "\n%s%s%s.%s = %s" % (indented, newNode, propertyActions, propertyName, repr(value))
-            elif propertyAction == "attribute":
-                python += "\n%s%s%s.attributes[%s] = %s" % (indented, newNode, propertyActions, repr(propertyName),
-                                                            repr(value))
-            elif propertyAction == "javascriptEvent":
-                python += "\n%s%s%s.addJavascriptEvent(%s, %s)" % (indented, newNode, propertyActions,
-                                                                   repr(propertyName), repr(value))
-            elif propertyAction == "call":
-                if value:
-                    python += "\n%s%s%s.%s()" % (indented, newNode, propertyActions, propertyName)
-            elif propertyAction == "send":
-                python += "\n%s%s%s.%s(%s, %s)" % (indented, newNode, propertyActions,
-                                                                   propertyName, repr(name), repr(value))
-            elif propertyAction == "addClassesFromString":
-                python += "\n%s%s%s.addClasses(%s)" % (indented, newNode, propertyActions,
-                                                       repr(tuple(value.split(" "))))
-            elif propertyAction == "setStyleFromString":
-                python += "\n%s%s%s.style.update(%s)" % (indented, newNode, propertyActions,
-                                                         repr(StyleDict.fromString(value)))
-            else:
-                python += "\n%s%s%s.%s(%s)" % (indented, newNode, propertyActions, propertyAction, repr(value))
-
-    if accessor:
-        accessors_used.add(accessor)
-        python += "\n%stemplate.%s = %s" % (indented, accessor, newNode)
-
-    if children:
-        if isCached:
-            childAccessors = set()
-            childIndent = indent + 1
-        else:
-            childAccessors = accessors_used
-            childIndent = indent
-        for node in children:
-            (childPython, instance) = __createPythonFromTemplate(node, factory, newNode, instance, elements_used,
-                                                            childAccessors, cache_elements, static_elements, childIndent)
-            python += childPython
-        if isCached:
-            if childAccessors:
-                accessors_used.update(childAccessors)
-                python += "\n%selse:" % indented
-                for accessor in childAccessors:
-                    python += "\n%stemplate.%s = %s" % (indented + INDENT, accessor, newNode)
-
-
-    python += "\n%s%s.add(%s, ensureUnique=False)" % (indented, parent_node, newNode)
-    if parent_node == "template":
-        defineElements = ""
-        for elementName in elements_used:
-            variableName = elementName.replace("-", "_")
-            if variableName in ("and", "or", "with", "if", "del", "template"):
-                variableName = "_" + variableName
-            defineElements += "globals()['%s'] = products['%s']\n%s" % (variableName, elementName, INDENT * 2)
-
-        cacheDefinitions = ""
-        for elementName in cache_elements:
-            cacheDefinitions += "%s = CacheElement()\n" % elementName
-
-        return SCRIPT_TEMPLATE % {'accessors':tuple(accessors_used), 'buildTemplate':python,
-                                  'defineElements':defineElements, 'cache_elements':cacheDefinitions,
-                                  'static_elements':"\n".join(static_elements)}
-
-
-    return (python, instance)
