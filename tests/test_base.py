2d6bc0587fab3b58bf3ec5be515baa11be3be707
Timothy Crosley <timothy.crosley@gmail.com>
2016-01-04 19:52:38 -0800

    Try alternative way of adding to list

--- tests/test_base.py
+++ tests/test_base.py
@@ -103,7 +103,7 @@ class TestBlox(TestBlok):
 
         for_testing = Text('for testing')
         old_size = len(self.blok)
-        self.blok += for_testing
+        self.blok.blox.append(for_testing)
         assert self.blok[old_size] == for_testing
         assert len(self.blok) == old_size + 1
         assert for_testing in self.blok
