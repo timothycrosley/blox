15ba14e0eb49095d3feaeec626f76c5e725c692d
Timothy Crosley <timothy.crosley@gmail.com>
2017-10-04 17:17:34 -0700

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ except (IOError, ImportError, OSError, RuntimeError):
    readme = ''
 
 setup(name='blox',
-      version='0.3.1',
+      version='0.3.2',
       description='Build it with Python',
       long_description=readme,
       author='Timothy Crosley',
